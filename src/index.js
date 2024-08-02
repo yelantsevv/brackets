060e10880a83f1208e0d91f14ab579496f9ed993
yelantsevv <yelantsevv@gmail.com>
2024-08-02 13:55:19 +0300

    fix: test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketsAdd = bracketsConfig.map((el) => el[0]);
+  const bracketsDel = bracketsConfig.map((el) => el[1]);
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (bracketsAdd.includes(char)) {
+      if (bracketsDel.includes(char) && char == stack.at(-1)) {
+        stack.pop();
+        continue;
+      }
+      stack.push(char);
+    } else if (bracketsDel.indexOf(char) == bracketsAdd.indexOf(stack.at(-1))) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  return stack.length === 0;
+};
